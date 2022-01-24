556904c1f85f764cc270d152af216f4767b6a448
Jordan <88350421+Hazey8709@users.noreply.github.com>
2022-01-24 17:00:31 -0500

    Add files via upload

---
+++ Week-4/styled-website/jav-about.js
@@ -0,0 +1,13 @@
+//  JavaScript About Page
+
+var typed = new Typed(".auto-about", {
+    strings: [
+        "Hey  Noobie's",
+        "Welcome to Noobie J's",
+        " This is the about page. My name is Jordan this website is an assignment for class. On my site you will find Info on projects/work I've done in school and also for practice.",
+        "Hope you enjoy! It's a work in progress.  Thank you for the visit.",
+    ],
+    typeSpeed: 100,
+    backspeed: 50,
+    loop: false,
+});
---
+++ Week-4/styled-website/jav-home.js
@@ -0,0 +1,13 @@
+//  JavaScript
+
+var typed = new Typed(".auto-input", {
+    strings: [
+        "Welcome  Noobie's",
+        "My name is Jordan this is my website Noobie J's",
+        "A work in progress,",
+        " I'm a Web Developer in school for Associates Degree in Full Stack Web Development at Bryan University",
+    ],
+    typeSpeed: 100,
+    backspeed: 50,
+    loop: true,
+});
